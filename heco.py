239961c756390cdf77af4f0ef3d1af48380ba6c1
remloveu <remloveu@163.com>
2021-11-08 19:00:57 +0800

    11.8

--- heco.py
+++ heco.py
@@ -18,6 +18,8 @@ import time
 from queue import Queue
 import pymongo.errors
 from threading import Thread
+import web3.exceptions
+import asyncio
 
 # 连接mongodb
 client = pymongo.MongoClient('127.0.0.1', 27017)
@@ -36,6 +38,8 @@ approve = db.approve
 sell = db.sell
 # 记录用户分成收益集合
 profit = db.profit
+# 记录更新到的块
+block = db.block
 
 w3 = Web3(Web3.HTTPProvider('https://http-mainnet-node.huobichain.com'))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
@@ -93,7 +97,9 @@ event_dict = {
     'control_token':
     '0xfc1e08f776282b1abf0734388c9042ad8206984bf5d69a721d120c9af38412fc'
 }
-
+block_num = block.count_documents({'_id':1})
+if block_num == 0:
+    block.insert_one({'_id':1,'block':w3.eth.get_block_number()})
 
 
 # 生成metadata
@@ -165,7 +171,7 @@ def pin_file_to_ipfs():
             f.close()
         time.sleep(3)
 
-    
+
 def pin_str_to_ipfs():
     while 1:
         if not p.empty():
@@ -436,7 +442,7 @@ def handle(event_list):
                     event_type = 'burn'
                 else:
                     works.update_one(
-                        {'_id': token_id}, 
+                        {'_id': token_id},
                         {'$set': {
                             'owner': ad_to
                         }})
@@ -483,6 +489,11 @@ def handle(event_list):
                     })
                 except Exception:
                     pass
+            works.update_one({'_id': token_id},
+                             {'$set': {
+                                 'state1': 0,
+                                 'state2': 0
+                             }})
         elif topics[0].hex() == event_dict['auction_set']:
             token_id = int(data[:66], 16)
             time_begin = int(data[66:130], 16)
@@ -534,15 +545,124 @@ def handle(event_list):
 
 # 事件循环
 def log_loop():
-    filt = w3.eth.filter({'address': address, 'fromBlock': '0x0'})
     while 1:
         try:
-            event_list = w3.eth.get_filter_changes(filt.filter_id)
-            if not len(event_list) == 0:
-                handle(event_list)
-        except ValueError:
-            filt = w3.eth.filter({'address': address, 'fromBlock': '0x0'})
-        time.sleep(2)
+            while 1:
+                doc = block.find_one({'_id':1})
+                fromBlock = doc['block']
+                toBlock = w3.eth.block_number
+                try:
+                    event_list = w3.eth.getLogs({'address': address, 'fromBlock': fromBlock,'toBlock':toBlock})
+                    if not len(event_list) == 0:
+                        handle(event_list)
+                except Exception as e:
+                    print(e)
+                fromBlock = toBlock+1
+                block.update_one({'_id':1},{'$set':{'block':fromBlock}})
+                time.sleep(3)
+        except Exception as e:
+            print(e)
+        time.sleep(3)
+
+
+# 判断token是否在链上
+def judge(token_id):
+    try:
+        contract.functions.tokenURI(token_id).estimateGas()
+        return True
+    except Exception as e:
+        return False
+
+
+# 数据库更新
+async def up(doc):
+    token_id = doc['_id']
+    try:
+        owner_gas = contract.functions.ownerOf(token_id).estimateGas()
+        owner = contract.functions.ownerOf(token_id).call({'gas': owner_gas})
+        if owner == black_address:
+            works.delete_one({'_id': token_id})
+            return
+        if judge(token_id):
+            if doc['type'] == 'single':
+                flag = True
+            elif doc['type'] == 'canvas':
+                layer_token = doc['data']
+                flag = True
+                for i in layer_token:
+                    flag = flag and judge(i)
+            else:
+                layer_json = doc['json_data']
+                layer_json = json.loads(layer_json)
+                canvas_token_id = layer_json['canvas_token_id']
+                canvas_doc = works.find_one({'_id': canvas_token_id})
+                flag = canvas_doc['flag']
+        else:
+            flag = False
+        state_gas = contract.functions.sellingState(token_id).estimateGas()
+        res = contract.functions.sellingState(token_id).call(
+            {'gas': state_gas})
+        buy_price = str(res[0])
+        start_price = str(res[1])
+        max_price = '0'
+        end_price = '0'
+        auction_start_time = res[2]
+        auction_end_time = res[3]
+        if res[0] == 0:
+            state1 = 0
+        else:
+            state1 = 1
+        if res[1] == 0:
+            state2 = 0
+        else:
+            t = time.time()
+            now_time = int(t)
+            if now_time < res[2]:
+                state2 = 1
+            elif not now_time < res[2] and not now_time > res[3]:
+                state2 = 2
+                pending_gas = contract.functions.pendingBids(
+                    token_id).estimateGas()
+                pending = contract.functions.pendingBids(token_id).call(
+                    {'gas': pending_gas})
+                max_price = str(pending[1])
+            else:
+                state2 = 3
+                pending_gas = contract.functions.pendingBids(
+                    token_id).estimateGas()
+                pending = contract.functions.pendingBids(token_id).call(
+                    {'gas': pending_gas})
+                end_price = str(pending[1])
+        works.update_one({'_id': token_id}, {
+            '$set': {
+                'owner': owner,
+                'state1': state1,
+                'state2': state2,
+                'auction_start_time': auction_start_time,
+                'auction_end_time': auction_end_time,
+                'buy_price': buy_price,
+                'start_price': start_price,
+                'max_price': max_price,
+                'end_price': end_price,
+                'flag': flag,
+            }
+        })
+    except web3.exceptions.ContractLogicError:
+        pass
+    except Exception as e:
+        server.logger.exception('token_id:{} {}'.format(token_id, e))
+
+
+# 更新数据库内容
+def update():
+    while 1:
+        cursor = works.find()
+        new_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(new_loop)
+        loop = asyncio.get_event_loop()
+        tasks = [up(doc) for doc in cursor]
+        loop.run_until_complete(asyncio.wait(tasks))
+        time.sleep(10)
 
 
 # 获取token_id
@@ -1158,3 +1278,6 @@ t2.start()
 t3 = Thread(target=log_loop)
 t3.setDaemon(True)
 t3.start()
+t4 = Thread(target=update)
+t4.setDaemon(True)
+t4.start()
